7fde5a6a60db730d0e2e153358fbd8e62841dcfe
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-08-05 11:47:25 +0530

    Added day-8 starRating and progrssBar

--- src/components/ProgressBar/MyProgress.jsx
+++ src/components/ProgressBar/MyProgress.jsx
@@ -1,26 +1,42 @@
 
+// import ProgressBar from "./ProgressBar";
+
+// const MyProgress = () => {
+//   const handleProgressComplete = () => {
+//     console.log("Progress Completed!");
+//   };
+
+//   return (
+//     <div>
+//       <ProgressBar
+//         progress={50}
+//         direction="horizontal"
+//         onProgressComplete={handleProgressComplete}
+//       />
+//       <ProgressBar
+//         progress={75}
+//         direction="vertical"
+//         onProgressComplete={handleProgressComplete}
+//       />
+//     </div>
+//   );
+// };
+
+// export default MyProgress;
+
 import ProgressBar from "./ProgressBar";
 
-const MyProgress = () => {
-  const handleProgressComplete = () => {
-    console.log("Progress Completed!");
-  };
-
+function MyProgress() {
   return (
-    <div>
-      <ProgressBar
-        progress={50}
-        direction="horizontal"
-        onProgressComplete={handleProgressComplete}
-      />
-      <ProgressBar
-        progress={75}
-        direction="vertical"
-        onProgressComplete={handleProgressComplete}
-      />
+    <div className="App">
+      <h3 className="heading">Progress Bar</h3>
+      <ProgressBar bgcolor="orange" progress="30" height={30} />
+      <ProgressBar bgcolor="red" progress="60" height={30} />
+      <ProgressBar bgcolor="#99ff66" progress="50" height={30} />
+      <ProgressBar bgcolor="#ff00ff" progress="85" height={30} />
+      <ProgressBar bgcolor="#99ccff" progress="95" height={30} />
     </div>
   );
-};
+}
 
 export default MyProgress;
-
--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,60 +1,101 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+// import propTypes from "prop-types"
+
+// const ProgressBar = ({ progress, direction, onProgressComplete }) => {
+//   const [currentProgress, setCurrentProgress] = useState(0);
+
+//   useEffect(() => {
+//     setCurrentProgress(progress);
+//     if (progress === 100) {
+//       onProgressComplete();
+//     }
+//   }, [progress, onProgressComplete]);
+
+//   const getProgressBarStyles = () => {
+//     if (direction === "vertical") {
+//       return {
+//         height: "100%",
+//         width: "10px",
+//         backgroundColor: "#ddd",
+//         borderRadius: "5px",
+//       };
+//     } else {
+//       return {
+//         width: "100%",
+//         height: "10px",
+//         backgroundColor: "#ddd",
+//         borderRadius: "5px",
+//       };
+//     }
+//   };
+
+//   const getProgressFillStyles = () => {
+//     if (direction === "vertical") {
+//       return {
+//         height: `${currentProgress}%`,
+//         backgroundColor: "#4caf50",
+//         borderRadius: "5px",
+//       };
+//     } else {
+//       return {
+//         width: `${currentProgress}%`,
+//         backgroundColor: "#4caf50",
+//         borderRadius: "5px",
+//       };
+//     }
+//   };
+
+//   return (
+//     <div style={getProgressBarStyles()}>
+//       <div style={getProgressFillStyles()}></div>
+//     </div>
+//   );
+// };
+
+// ProgressBar.propTypes = {
+//   progress: propTypes.number,
+//   direction: propTypes.string,
+//   onProgressComplete:propTypes.func
+// };
+// export default ProgressBar;
+
 import propTypes from "prop-types"
-
-const ProgressBar = ({ progress, direction, onProgressComplete }) => {
-  const [currentProgress, setCurrentProgress] = useState(0);
-
-  useEffect(() => {
-    setCurrentProgress(progress);
-    if (progress === 100) {
-      onProgressComplete();
-    }
-  }, [progress, onProgressComplete]);
-
-  const getProgressBarStyles = () => {
-    if (direction === "vertical") {
-      return {
-        height: "100%",
-        width: "10px",
-        backgroundColor: "#ddd",
-        borderRadius: "5px",
-      };
-    } else {
-      return {
-        width: "100%",
-        height: "10px",
-        backgroundColor: "#ddd",
-        borderRadius: "5px",
-      };
-    }
+const ProgressBar = ({ bgcolor, progress, height }) => {
+  const Parentdiv = {
+    height: height,
+    width: "100%",
+    backgroundColor: "whitesmoke",
+    borderRadius: 40,
+    marginTop:"20px"
   };
 
-  const getProgressFillStyles = () => {
-    if (direction === "vertical") {
-      return {
-        height: `${currentProgress}%`,
-        backgroundColor: "#4caf50",
-        borderRadius: "5px",
-      };
-    } else {
-      return {
-        width: `${currentProgress}%`,
-        backgroundColor: "#4caf50",
-        borderRadius: "5px",
-      };
-    }
+  const Childdiv = {
+    height: "100%",
+    width: `${progress}%`,
+    backgroundColor: bgcolor,
+    borderRadius: 40,
+    textAlign: "right",
+  };
+
+  const progresstext = {
+    padding: 10,
+    color: "black",
+    fontWeight: 900,
   };
 
   return (
-    <div style={getProgressBarStyles()}>
-      <div style={getProgressFillStyles()}></div>
+    <div style={Parentdiv}>
+      <div style={Childdiv}>
+        <span style={progresstext}>{`${progress}%`}</span>
+      </div>
     </div>
   );
 };
 
 ProgressBar.propTypes = {
-  progress: propTypes.number,
-  direction: propTypes.string,
-  onProgressComplete:propTypes.func
+  height: propTypes.number,
+  progress: propTypes.string,
+  bgcolor: propTypes.string,
 };
+
 export default ProgressBar;
